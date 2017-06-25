21fc70c8f284d9f2c88cd2c05129344e53fccabc
Francisco Cancelo García <cancelo.garcia@outlook.com>
2017-06-25 04:15:14 +0200

    Issue #1 - Implement DatabaseParser

---
+++ ShamelessMobile/ShamelessMobile/Resources/URLs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShamelessMobile.Resources
+{
+    public static class URLs
+    {
+        public static string Json_Enc = "http://3ds.titlekeys.gq/json_enc";
+        public static string Sizes = "http://housebreaker.net/sizes.json";
+    }
+}
---
+++ ShamelessMobile/ShamelessMobile/Services/DatabaseParser.cs
@@ -0,0 +1,21 @@
+﻿using ShamelessMobile.Resources;
+using ShamelessMobile.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShamelessMobile.Services
+{
+    public class DatabaseParser : IDatabaseParser
+    {
+        public async void DownloadDatabase(string outputFile)
+        {
+            HttpClient client = new HttpClient();
+            var result = await client.GetAsync(URLs.Json_Enc);
+            
+        }
+    }
+}
