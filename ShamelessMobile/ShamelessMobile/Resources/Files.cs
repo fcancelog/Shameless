bd3b6d029b3abe3e0975e3d5e54e621018708d26
Francisco Cancelo García <cancelo.garcia@outlook.com>
2017-06-25 04:15:48 +0200

    Minor changes: Added Files static class

---
+++ ShamelessMobile/ShamelessMobile/Resources/Files.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShamelessMobile.Resources
+{
+    public static class Files
+    {
+        public static string DBPath = "db.json";
+        public static string SizesPath = "sizes.json";
+    }
+}
