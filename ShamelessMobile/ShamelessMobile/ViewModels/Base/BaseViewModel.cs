1023075ee23d971e2903e211943a53f07740a35d
Francisco Cancelo García <cancelo.garcia@outlook.com>
2017-06-27 14:06:46 +0200

    Added BaseViewModel with INotiftyPropertyChanged and Navigation

---
+++ ShamelessMobile/ShamelessMobile/ViewModels/Base/BaseViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace ShamelessMobile.ViewModels.Base
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public INavigation Navigation { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void Raise(string propertyName)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
