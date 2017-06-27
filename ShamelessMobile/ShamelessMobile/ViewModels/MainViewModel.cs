e435a0e331c11e50611c45320d39de98c706f5ee
Francisco Cancelo García <cancelo.garcia@outlook.com>
2017-06-27 14:07:25 +0200

    Added MainView(Testing purposes only)

---
+++ ShamelessMobile/ShamelessMobile/ViewModels/MainViewModel.cs
@@ -0,0 +1,36 @@
+﻿using ShamelessMobile.Resources;
+using ShamelessMobile.Services;
+using ShamelessMobile.Services.Interfaces;
+using ShamelessMobile.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace ShamelessMobile.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        IDatabaseParser _databaseParser;
+
+        public MainViewModel()
+        {
+            _databaseParser = new DatabaseParser();
+            string a = String.Empty;
+        }
+
+        public ICommand CommandTest
+        {
+            get
+            {
+                return new Command(() =>
+               {
+                   _databaseParser.DownloadDatabase(Files.DBFileName);
+               });
+            }
+        }
+    }
+}
---
+++ ShamelessMobile/ShamelessMobile/Views/MainView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using ShamelessMobile.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ShamelessMobile.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainView : ContentPage
+    {
+        public MainView()
+        {
+            InitializeComponent();
+            BindingContext = new MainViewModel();
+        }
+    }
+}
